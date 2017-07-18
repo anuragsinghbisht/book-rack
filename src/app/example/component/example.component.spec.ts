bad5f40807c2ce9ab9480deef2e5e961d85a65c1
anuragsinghbisht <anurag.bisht@imaginea.com>
2017-07-18 22:47:03 +0530

    Fix failing test cases

--- src/app/example/component/example.component.spec.ts
+++ src/app/example/component/example.component.spec.ts
@@ -1,17 +1,22 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { StoreModule } from '@ngrx/store';
+import { reducer } from '../../app.reducers';
+import { ExampleActions } from '../actions/example.actions';
 import { ExampleComponent } from './example.component';
 
 describe('ExampleComponent', () => {
   let component: ExampleComponent;
   let fixture: ComponentFixture<ExampleComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ExampleComponent ]
+  beforeEach(
+    async(() => {
+      TestBed.configureTestingModule({
+        imports: [StoreModule.provideStore(reducer)],
+        declarations: [ExampleComponent],
+        providers: [ExampleActions]
+      }).compileComponents();
     })
-    .compileComponents();
-  }));
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ExampleComponent);
